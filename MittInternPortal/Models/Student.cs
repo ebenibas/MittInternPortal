833ca1f649c8ede9f63bae218cb23ab397c2a221
Ebenezer Obaseki <ebenibas@outlook.com>
2019-10-21 13:45:50 -0400

    Add project files.

---
+++ MittInternPortal/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MittInternPortal.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser StudentName { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public int Contact { get; set; }
+
+        public string InstructorsId { get; set; }
+        public virtual ApplicationUser Instructor { get; set; }
+
+        public virtual ICollection<StudentJobPost> StudentJobPosts { get; set; }
+    }
+}
